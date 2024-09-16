beefd987cd67ee30e43a3827412fa297955741c6
Boris Yakimov <boris.yakimov@itgix.com>
2024-09-16 22:58:07 +0300

    #126 add dynamic extension for rule variables

--- stateful_rule_groups.tf
+++ stateful_rule_groups.tf
@@ -8,20 +8,51 @@ resource "aws_networkfirewall_rule_group" "suricata_stateful_group" {
   capacity    = var.suricata_stateful_rule_group[count.index]["capacity"]
 
   rule_group {
+
+    rules_source {
+      rules_string = var.suricata_stateful_rule_group[count.index]["rules_file"]
+    }
+
+    #dynamic "rule_variables" {
+    #for_each = var.suricata_stateful_rule_group[count.index].rule_variables
+    #content {
+    #ip_sets {
+    #key = rule_variables.value.key
+    #ip_set {
+    #definition = [rule_variables.value.ip_set]
+    #}
+    #}
+    #}
+    #}
+
     dynamic "rule_variables" {
-      for_each = var.suricata_stateful_rule_group[count.index].rule_variables
+      for_each = [
+        for b in lookup(var.suricata_stateful_rule_group[count.index], "rule_variables", {}) : b
+        if length(b) > 1
+      ]
       content {
-        ip_sets {
-          key = rule_variables.value.key
-          ip_set {
-            definition = [rule_variables.value.ip_set]
+        dynamic "ip_sets" {
+          for_each = lookup(lookup(var.suricata_stateful_rule_group[count.index], "rule_variables", {}), "ip_sets", [])
+          content {
+            key = ip_sets.value["key"]
+            ip_set {
+              definition = ip_sets.value["ip_set"]
+            }
+          }
+        }
+
+        dynamic "port_sets" {
+          for_each = lookup(lookup(var.suricata_stateful_rule_group[count.index], "rule_variables", {}), "port_sets", [])
+          content {
+            key = port_sets.value["key"]
+            port_set {
+              definition = port_sets.value["port_sets"]
+            }
           }
         }
       }
     }
-    rules_source {
-      rules_string = var.suricata_stateful_rule_group[count.index]["rules_file"]
-    }
+
   }
 
   tags = merge(var.tags)
